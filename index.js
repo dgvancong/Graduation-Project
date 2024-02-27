80d82fbc26258877312e15037316bf9da5e46725
Dong Van Cong <dongvancong24@gmail.com>
2024-02-27 16:58:46 +0700

    project api

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require("express");
-const project = require("./routes/project");
-
-
+const projectRouter = require("./routes/project");
+const path = require('path');
+const cors = require('cors');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 const app = express();
-app.use(express.json());
 
+// Sử dụng middleware morgan để ghi lại các yêu cầu HTTP
+app.use(morgan('dev'));
+app.use(cors());
+app.use(cookieParser());
 
-app.use("/project", project);
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Sử dụng router cho các đường dẫn bắt đầu bằng /project
+app.use('/project', projectRouter);
 
 const port = process.env.PORT || 5000;
-app.listen(port,() => console.log(`Server is running at http://localhost:${port}`));
+app.listen(port, () => console.log(`Server is running at http://localhost:${port}`));
--- routes/project.js
+++ routes/project.js
@@ -1,11 +1,29 @@
 const express = require("express");
+var db = require('./dbConnect');
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
+// Lấy danh sách dự án
+router.get('/', function(req, res) {
+    const query = `
+      SELECT project.*, projectDetails.*,
+       user.fullName AS leadFullName ,
+       user.picture AS imgUser ,
+      team.teamName AS teamFullName
+      FROM project
+      LEFT JOIN projectDetails ON project.projectID = projectDetails.projectID
+      LEFT JOIN Users AS user ON projectDetails.userID = user.userID
+      LEFT JOIN Team AS team ON projectDetails.teamID = team.teamID
+    `;
+    db.query(query, function(err, result) {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Lỗi Server Nội Bộ");
+        } else {
+            res.status(200).json(result);
+        }
+    });
   });
-});
 
-module.exports = router;
+
+  var path = require('path');
+  module.exports = router;
